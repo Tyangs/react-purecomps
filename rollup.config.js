e219b0a125ff51ecf61e746c6e409443ba5ddfc0
tyangs <tyangs0324@gmail.com>
2023-04-12 15:01:16 +0800

    :sparkles: (pure-components) convert all react intrinsic-elements to pure-components

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
 
@@ -21,10 +20,5 @@ export default [
       },
     ],
     plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), terser()],
-  },
-  {
-    input: 'dist/esm/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()],
   },
 ];
