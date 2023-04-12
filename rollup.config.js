bc8f147e2ea94714859c5062d700198f201c2fdf
tyangs <tyangs0324@gmail.com>
2023-04-12 11:45:39 +0800

    :wrench: (rollup) add rollup config for build

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/cjs/index.ts',
+        format: 'cjs',
+        sourcemap: false,
+      },
+      {
+        file: 'dist/esm/index.ts',
+        format: 'esm',
+        sourcemap: false,
+      },
+    ],
+    plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), terser()],
+  },
+  {
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
+];
